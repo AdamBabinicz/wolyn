ddcd30b91453d64b4e0f715eccac868be2e41ca6
babinicz <babinicz@onet.pl>
2022-07-20 17:04:01 +0200

    ciasteczka

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   useEffect(() => {
@@ -13,6 +14,33 @@ function App() {
   return (
     <>
       <Pages />
+      <CookieConsent
+        debug={true}
+        location="bottom"
+        style={{
+          background: "#333",
+          textAlign: "left",
+          paddingBottom: "1rem",
+          fontSize: "16px",
+          fontFamily: "Gideon Roman",
+        }}
+        buttonStyle={{
+          color: "#333",
+          background: "#fff",
+          fontSize: "18px",
+          fontFamily: "Gideon Roman",
+          marginRight: "1rem",
+        }}
+        buttonText="OK, rozumiem"
+        expires={365}
+      >
+        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+        ustawień dotyczących cookies."
+      </CookieConsent>
     </>
   );
 }
